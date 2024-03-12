1d25232c6a05ebefc49cbb79417a49ac24733ca6
NMandel527 <nmandel527@gmail.com>
2024-03-12 09:51:37 -0400

    fixing random homework

--- src/main/java/mandel/fallingsand/Main.java
+++ src/main/java/mandel/fallingsand/Main.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Sand sand = new Sand(50, 10);
-        sand.randomSand(50);
         Scanner input = new Scanner(System.in);
+        sand.randomSand(50);
         while (true) {
-            System.out.println("enter");
+            System.out.println("Enter");
             String enter = input.nextLine();
             if (enter.isEmpty()) {
                 sand.fall();
